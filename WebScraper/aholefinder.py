f9e39316501743b9b12098636f785838148402b3
James Robinson <160552r@acadiau.ca>
2024-01-25 11:07:43 -0400

    Increased Volume, Dealt with emojis, Added error log file
    >Increased the number of unique posts to  appx. 7 500 at start with the potential for just under 2 000 new posts each week.
    >Added to the CleanText function to remove emojis and other unreadable chars.
    >Errors are now added to a .txt file corresponding to their category, timeframe, and the date scraped rather than printed to the console

--- WebScraper/aholefinder.py
+++ WebScraper/aholefinder.py
@@ -2,22 +2,9 @@
 # File:          aholefinder.py                                                                   #
 # Author:        James Robinson                                                                   #
 # Purpose:       Collect data from the subreddit r/AmItheAsshole to train a classifier.           #
-# Last Modified: 11/29/2023                                                                       #
-# TODO: Get more than 25 posts at a time? Deal with edge cases :(                                 #
+# Last Modified: 01/25/2024                                                                       #
+# TODO:          Set to run weekly                                                                #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Error Jail                                                                                                              #
-# Traceback (most recent call last):                                                                                      #
-#  File "c:\Users\james\Downloads\Projects\Scraper\WebScraper\AITA\aholefinder.py", line 57, in analyze_post              #
-#    filename = datetime.today().strftime('%Y-%m-%d') + '.json'                                                           #
-#            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                                                  #
-#  File "C:\Users\james\AppData\Local\Programs\Python\Python312\Lib\encodings\cp1252.py", line 19, in encode              #
-#    return codecs.charmap_encode(input,self.errors,encoding_table)[0]                                                    #
-#           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                                       #
-# UnicodeEncodeError: 'charmap' codec can't encode character '\U0001f480' in position 2422: character maps to <undefined> #
-# -its a skull emoji
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #imports-------------------------------------------------------------------------------------------
 from selenium import webdriver #To run chrome
@@ -31,86 +18,120 @@ import traceback #To debug better
 import time #To wait
 import random #To randomize wait times
 
-#Setup---------------------------------------------------------------------------------------------
+import re #To remove emojis
 
+def GetPosts(driver: webdriver, category: str, timeFrame: str) -> None:
+    """Uses Selenium Werbdriver to gather post URLs and pass them to AnalyzePost"""
+    jsonFile = OpenFile(category, timeFrame)
+    homeLink = 'https://old.reddit.com/r/AmItheAsshole/'+category+'/?sort='+category+'&t='+timeFrame
+    driver.get(homeLink)
+    numPosts, numPages = 0, 0
 
-#Ensures text pulled from posts is compatible with JSON
-def clean_text(text):
-    """Adds escape characters to double quotes.
-    Removes leading and trailing whitespace."""
-    clean_text = text.strip()
-    clean_text = clean_text.replace('"', '\\"')
-    return clean_text
+    nextPageButton = driver.find_element(By.CLASS_NAME, 'next-button')
+    while (nextPageButton):
+        #Get all posts on this page, go to the next page
+        numPages += 1
+        print ("\nPage " + str(numPages) + ":\n")
+        posts = driver.find_elements(By.CLASS_NAME, 'thing')
+        for post in posts:
+            numPosts += 1
+            print ("Post " + str(numPosts))
+            postURL = 'https://www.reddit.com' + (post.get_attribute('data-permalink'))
+            AnalyzePost(postURL, jsonFile)     
+        nextPageButton = GetNextPage(driver, nextPageButton)
+        
 
-#Analyze Post Function-----------------------------------------------------------------------------
-def analyze_post(post_link):
-    """Navigate to a specific post by link.
-    Get the title, post body, and public decision.
-    Store the above in a json file."""
+def GetNextPage(driver: webdriver, nextPageButton: webdriver) -> webdriver: #WebElement
+    """Navigates the driver to the next page.
+    Returns the new next page button.
+    Waits a random amount of time between pages."""
+    nextPageButton.click()
+    time.sleep(random.randrange(1, 50)/10)
+    try:
+        nextPageButton = driver.find_element(By.CLASS_NAME, 'next-button')
+    except:
+        nextPageButton = None #End of pages, ~40
+    return nextPageButton
 
-    #Get post
-    post = requests.get(post_link)
-    postsoup = BeautifulSoup(post.text, 'lxml')
+def AnalyzePost(postLink: str, filename: str) -> None:
+    """Navigate to a specific post by URL.
+    Return the post's title, body, and decision."""
+    post = requests.get(postLink)
+    postSoup = BeautifulSoup(post.text, 'lxml')
 
     #Parse for relevant data
     try:
-        title = clean_text(postsoup.find('h1').text)
-        paragraphs = postsoup.find_all('p')
+        title = cleanText(postSoup.find('h1').text)
+        paragraphs = postSoup.find_all('p')
         body = ""
         for paragraph in paragraphs[1:]:
-            body += clean_text(paragraph.text)
-        decision = clean_text(postsoup.find('shreddit-post-flair').text)
+            body += cleanText(paragraph.text)
+        decision = cleanText(postSoup.find('shreddit-post-flair').text)
+        print (decision)
         if (decision == ''): #Skip posts with no decision
             return
     except:
-        print ("Issue with post: " + post_link)
-        traceback.print_exc()
+        #Write error to error log file and continue
+        with open ('./errors/' + filename + '.txt', 'a') as e:
+            e.write(traceback.format_exc())
+        return
+    WriteToFile(title, body, decision, filename)
 
-    WriteToFile(title, body, decision)
+def cleanText(text: str) -> str:
+    """Adds escape characters to double quotes.
+    Removes leading and trailing whitespace.
+    Removes emojis."""
+    cleanText = text.strip()
+    cleanText = cleanText.replace('"', '\\"')
 
-#Write To File Function---------------------------------------------------------------------------
-def WriteToFile(title, body, decision):
-    """Writes a post's data as a json entry in today's file."""
-    filename = datetime.today().strftime('%Y-%m-%d') + '.json'
+    #Removes emojis based on their python encoding, will add more as they come up
+    emojiPattern = re.compile("["
+            u"\U0001F600-\U0001F64F"  # Emoticons
+            u"\U0001F300-\U0001F5FF"  # Symbols and Pictographs
+            u"\U0001F680-\U0001F6FF"  # Transport and Map Symbols
+            u"\U0001F1E0-\U0001F1FF"  # Flags (iOS)
+            u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+            u"\u2060"                 # Word Joiner
+                            "]+", flags=re.UNICODE)
+    cleanText = (emojiPattern.sub(r'', cleanText))
+
+    return cleanText
+
+def WriteToFile(title: str, body: str, decision: str, filename: str) -> None:
+    """Writes a post's data as a json entry in the proper file."""
     with open('./data/' + filename, 'a') as f:
         try:
+            #Write post data to file as a new json entry
             f.write("\n{\n\t\"title\" : \"" + title + "\",")
             f.write("\n\t\"body\" : \"" + body + "\",")
-            if ('\U0001f4a9' in decision): #This handles decisions of 'Poo Mode' which include an unreadable character (poo emoji)
-                decision = 'POO Mode'
             f.write("\n\t\"decision\" : \"" + decision + "\",")
             f.write("\n\t\"valid\" : \"true\" \n},")
-        except: #This takes care of crash-causing edge cases, mostly unencodable chars atm
-            f.write("\n\t\"valid\" : \"false\" \n},") #Since exceptions occur in the body typically, this maintains the JSON file format
-            print("")
-            traceback.print_exc()
-            print("")
+        except:
+            #Write error to error log file and continue
+            f.write("\n\t\"valid\" : \"false\" \n},")
+            with open('./errors/' + filename + '.txt', 'a') as e:
+                e.write(traceback.format_exc())
 
-def GetPosts(driver, category, timeFrame):
-    homeLink = 'https://old.reddit.com/r/AmItheAsshole/?sort='+category+'&t='+timeFrame
-    driver.get(homeLink)
-    nextPageButton = driver.find_element(By.CLASS_NAME, 'next-button')
-    while (nextPageButton):
-        nextPageButton = driver.find_element(By.CLASS_NAME, 'next-button')
-        divs = driver.find_elements(By.CLASS_NAME, 'thing')
-        for div in divs:
-            isAnAdd = ('user' in div.get_attribute('data-permalink'))
-            if (isAnAdd):
-                print("Skipping ad")
-                break
-            analyze_post('https://www.reddit.com' + (div.get_attribute('data-permalink')))
-        nextPageButton.click()
-        time.sleep(random.randrange(1, 50)/10)
+def OpenFile(category: str, timeFrame: str) -> str:
+    """Opens a new json file for the given category, timeframe, and today's date."""
+    filename = datetime.today().strftime('%Y-%m-%d') + '_' + category + '_' + timeFrame + '.json'
+    f = open('./data/' + filename, 'w')
+    with open('./data/' + filename, 'w') as f:
+        f.write("[")
+    return filename
 
 #Main----------------------------------------------------------------------------------------------
-filename = datetime.today().strftime('%Y-%m-%d') + '.json'
-f = open('./data/' + filename, 'w')
-with open('./data/' + filename, 'w') as f:
-    f.write("[")
-
 driver = webdriver.Chrome()
-GetPosts(driver, 'top', 'all')
-driver.quit()
+#GetPosts(driver, 'top', 'all')
+#GetPosts(driver, 'controversial', 'all')
 
-with open('./data/' + filename, 'a') as f:
-    f.write("\n]\n")
+#GetPosts(driver, 'top', 'year')
+#GetPosts(driver, 'controversial', 'year')
+
+#GetPosts(driver, 'top', 'month')
+#GetPosts(driver, 'controversial', 'month')
+
+GetPosts(driver, 'top', 'week')
+GetPosts(driver, 'controversial', 'week')
+
+driver.quit()
